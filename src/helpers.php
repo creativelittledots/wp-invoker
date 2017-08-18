f5044d872b02e39949aace3d154e260c74217ce4
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-08-18 10:17:23 +0100

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,4 @@
 <?php
-	
-	if( ! defined( 'DS' ) ) {
-		
-		define( 'DS', DIRECTORY_SEPARATOR );
-		
-	}
 
 	/*----------------------------------------------*\
     	#INVOKE FUNCTION
@@ -15,22 +9,6 @@
 	    function invoke( $callback, $action = 'wp', $condition = null, $priority = null ) {
 		    
 		    return WPKit\Invoker\Facades\Invoker::match( $callback, $action, $condition, $priority );
-		    
-	    }
-	    
-	}
-	
-	if ( ! function_exists('get_asset') ) {
-    
-	    function get_asset( $file, $server_path = false ) {
-		    
-		    if( file_exists( get_stylesheet_directory() . DS . $file ) ) {
-		            
-	            $path = $server_path ? get_stylesheet_directory() : get_stylesheet_directory_uri();
-                
-                return $path . DS . $file;
-                
-            }
 		    
 	    }
 	    
